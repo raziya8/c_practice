f264785799e959f421a861c6622fb17bd5664105
raziya8 <raziya.parween.info@gmail.com>
2023-01-03 00:34:20 +0530

    learn c program of division of two num.
     division of two numbers using if else statements.

---
+++ basic/div_of_two_if_else.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	int a = 10, b = 0, c = 0;
+	if (b != 0)
+	{
+		c = a/b ;
+		printf("div = %d \n", c);
+	}
+	else
+	{
+		printf(" Denominator is zero\n");
+	}
+	return 0;
+
+}
+
---
+++ basic/div_of_two_num.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+	int a = 10, b = 2, c = 0;
+	c = a/b;
+	printf("division of = %d\n", c);
+	return 0;
+}
