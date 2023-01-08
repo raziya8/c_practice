05274598f4a3aca233278dd89cc979144ef94e6b
raziya8 <raziya.parween.info@gmail.com>
2023-01-08 11:42:31 +0530

    write c program to take input of four numbers

---
+++ basic/take_input_of_four_num.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	int num1, num2 ,num3 ,num4;
+	printf("\nprint number1=");
+	scanf("\n%d\n", &num1);
+	printf("\nprint number2 =");
+        scanf("%d\n", &num2);
+	printf("\nprint number3=");
+        scanf("\n%d\n", &num3);
+	printf("\nprint number4=");
+        scanf("\n%d\n", &num4);
+	return 0;
+}
